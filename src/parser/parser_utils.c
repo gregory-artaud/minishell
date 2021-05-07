42288f7c76dda334be1016e21e7de6d2ba683cc1
Thomas Simon <tsimon@student.42lyon.fr>
2021-05-07 11:40:42 +0200

    free done

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -1,62 +1,74 @@
 #include "minishell.h"
 
-static	char	*ft_strncpy(char *dest, char *src, int n)
+int	ft_nb_arg(t_shell sh, t_token *token)
 {
 	int	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (token->type == ARGUMENT && sh.tokens)
 	{
-		dest[i] = src[i];
 		i++;
+		sh.tokens = sh.tokens->next;
+		if (sh.tokens)
+			token = sh.tokens->content;
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (i);
 }
 
 char	*my_strdup(char *src, int n)
 {
-	char *str;
+	char	*str;
 
-	str = (char*)malloc(sizeof(*str) * n + 1);
+	str = (char *)malloc(sizeof(*str) * n + 1);
 	if (str == NULL)
 		return (0);
 	ft_strncpy(str, src, n);
 	return (str);
 }
 
-int	ft_strlen_sp(char *str, char c)
-{
-	int i;
-
-	i = 0;
-	while (str[i] && str[i] != ' ' && str[i] != c)
-		i++;
-	return (i);
-}
-
-void	ft_errorzsh(char *cmd)
-{
-	printf("zsh: command not found: %s\n", cmd);
-}
-
 int	my_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
-    int             result;
+	int				result;
 
 	i = 0;
 	while (s1[i] && ((s1[i] == s2[i] || (((unsigned char)s1[i])
-        - ((unsigned char)s2[i])) == 32)) && (i < n - 1))
+					- ((unsigned char)s2[i])) == 32)) && (i < n - 1))
 		i++;
-    if (s1[i + 1] != 0 || s2[i + 1] != 0)
-        return (-1);
-    result = ((unsigned char)s1[i]) - ((unsigned char)s2[i]);
-    if (result == 32)
-        result = 0;
+	if (s1[i + 1] != 0 || s2[i + 1] != 0)
+		return (-1);
+	result = ((unsigned char)s1[i]) - ((unsigned char)s2[i]);
+	if (result == 32)
+		result = 0;
 	if (n)
 		return (result);
 	return (0);
+}
+
+void	fill_new_branche(t_token *token, t_tree **tree)
+{
+	t_tree	*next;
+
+	if ((*tree)->branches == NULL)
+	{
+		ft_tr_addleft(*tree, ft_tr_new(token->value, token->type, 0));
+		*tree = (*tree)->branches->content;
+	}
+	else
+	{
+		next = (*tree)->branches->content;
+		if (ft_strncmp(next->content, ";", 1) == 0
+			|| ft_strncmp(next->content, "|", 1) == 0)
+		{
+			ft_tr_addleft(*tree, ft_tr_new(token->value, token->type, 0));
+			(*tree) = (*tree)->branches->content;
+		}
+		else
+		{
+			ft_tr_addright(*tree, ft_tr_new(token->value, token->type, 0));
+			(*tree) = (*tree)->branches->next->content;
+		}
+	}
 }
 
 int	ft_verif_builtin(char *builtin)
@@ -70,11 +82,11 @@ int	ft_verif_builtin(char *builtin)
 		&& my_strncmp("unset", builtin, len) != 0
 		&& my_strncmp("env", builtin, len) != 0
 		&& my_strncmp("exit", builtin, len) != 0)
-    {
-        if (ft_strncmp(builtin, "./", 2) == 0)
-            return (EXIT_SUCCESS);
-        else
-		    return (EXIT_FAILURE);
-    }
+	{
+		if (ft_strncmp(builtin, "./", 2) == 0)
+			return (EXIT_SUCCESS);
+		else
+			return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
