0189275214660018b44de9976b5b610ed282b29e
Thomas Simon <tsimon@student.42lyon.fr>
2021-05-07 11:39:30 +0200

    add strncpy

--- lib/libft/tree/tree.h
+++ lib/libft/tree/tree.h
@@ -12,14 +12,16 @@ typedef struct s_list
 typedef struct s_tree
 {
 	void			*content;
+	int				type;
 	struct s_tree	*parent;
 	t_list			*branches;
+	int				size;
 }					t_tree;
 
 /*
 ** tree
 */
-t_tree	*ft_tr_new(void *content);
+t_tree	*ft_tr_new(void *content, int type, int size);
 void	ft_tr_addleft(t_tree *root, t_tree *left);
 void	ft_tr_addright(t_tree *root, t_tree *right);
 int		ft_tr_isleaf(t_tree *tr);
@@ -30,14 +32,14 @@ t_tree	*ft_tr_next_sibling(t_tree *tr);
 /*
 ** list
 */
-t_list				*ft_lstnew(void *content);
-void				ft_lstadd_front(t_list **alst, t_list *new);
-int					ft_lstsize(t_list *lst);
-t_list				*ft_lstlast(t_list *lst);
-void				ft_lstadd_back(t_list **alst, t_list *new);
-void				ft_lstdelone(t_list *lst, void (*del)(void *));
-void				ft_lstclear(t_list **lst, void (*del)(void *));
-void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **alst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **alst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),
+			void (*del)(void *));
 #endif
