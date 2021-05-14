b1d9cda7812a32289aedc5b1b1ce8048db8a0e77
Wazack <64Anex@gmail.com>
2021-05-14 12:28:53 +0200

    fix: -n echo

--- src/executer/builtins/b_echo.c
+++ src/executer/builtins/b_echo.c
@@ -4,14 +4,25 @@ void	print_echo(t_tree *arg, int fd)
 {
 	char	**tableau;
 	int		i;
+	int		option;
 
 	i = 0;
 	tableau = arg->content;
+	option = 0;
+	if (!ft_strncmp("-n", tableau[i], 2))
+	{
+		option = 1;
+		i++;
+	}
 	while (tableau[i])
 	{
 		write(fd, tableau[i], ft_strlen(tableau[i]));
+		if (tableau[i + 1])
+			write(fd, " ", 1);
 		i++;
 	}
+	if (!option)
+		write(fd, "\n", 1);
 }
 
 void	display_echo(t_tree *redir, t_tree *arg)
@@ -35,9 +46,14 @@ int	b_echo(void *sh, t_tree *root)
 	{
 		tmp = root->branches->content;
 		if (tmp->type == REDIRECT)
-			create_file_redirect(tmp);
+		{
+			fd = create_file_redirect(tmp);
+			write(fd, "\n", 1);
+		}
 		else if (root->branches->next)
 			display_echo(root, tmp);
+		else
+			print_echo(tmp, 1);
 	}
 	else
 		write(fd, "\n", 1);
