5e0e7d764af4b637f04153e404e7c33d2c2816c5
Grégory Artaud <gregory.rtd@gmail.com>
2021-06-05 22:47:56 +0200

    fix: overwrite output redirects

--- src/executer/redirect_utils.c
+++ src/executer/redirect_utils.c
@@ -11,11 +11,11 @@ int	does_file_exists(char *f)
 	return (1);
 }
 
-int	create_file(char *file)
+int	create_file(char *file, int mode)
 {
 	int	fd;
 
-	fd = open(file, O_RDONLY | O_CREAT, 0644);
+	fd = open(file, O_RDONLY | O_CREAT | mode, 0644);
 	if (fd == -1)
 	{
 		printf("minishell: %s: %s\n", file, strerror(errno));
@@ -25,11 +25,42 @@ int	create_file(char *file)
 	return (EXIT_SUCCESS);
 }
 
+int	check_input(char **files, int i)
+{
+	int	error;
+
+	error = 0;
+	if (!ft_memcmp(files[i], REDIRECT_INPUT_TOKEN, R_I_T_LEN + 1))
+		error = !does_file_exists(files[i + 1]);
+	if (error)
+		printf("minishell: %s: %s\n", files[i + 1], strerror(errno));
+	return (error);
+}
+
+int	check_append(char **files, int i)
+{
+	int	error;
+
+	if (ft_memcmp(files[i], REDIRECT_OUTPUT_APPEND_TOKEN, R_O_A_T_LEN + 1))
+		return (0);
+	error = create_file(files[i + 1], 0);
+	return (error);
+}
+
+int	check_output(char **files, int i)
+{
+	int	error;
+
+	if (ft_memcmp(files[i], REDIRECT_OUTPUT_TOKEN, R_O_T_LEN + 1))
+		return (0);
+	error = create_file(files[i + 1], O_TRUNC);
+	return (error);
+}
+
 int	create_files(t_tree *tr)
 {
 	char	**files;
 	int		i;
-	char	*current;
 	int		error;
 
 	if (!tr)
@@ -39,15 +70,13 @@ int	create_files(t_tree *tr)
 	while (i < tr->size)
 	{
 		error = 0;
-		current = files[i];
-		if (!ft_memcmp(current, REDIRECT_INPUT_TOKEN, R_I_T_LEN + 1))
-			error = !does_file_exists(files[i + 1]);
+		error = check_input(files, i);
 		if (error)
-		{
-			printf("minishell: %s: %s\n", files[i + 1], strerror(errno));
 			return (error);
-		}
-		error = create_file(files[i + 1]);
+		error = check_append(files, i);
+		if (error)
+			return (error);
+		error = check_output(files, i);
 		if (error)
 			return (error);
 		i += 2;
